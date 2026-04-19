18c473d594679657bf6c798a4b4019f0e1baa396
sbadaspor <danielcostasilva.dcs@gmail.com>
2026-04-19 10:36:23 +0000

    fix: vapid key type

--- src/components/ui/NotificationButton.tsx
+++ src/components/ui/NotificationButton.tsx
@@ -3,7 +3,7 @@
 import { useState, useEffect } from 'react'
 import { createClient } from '@/lib/supabase/client'
 
-function urlBase64ToUint8Array(base64String: string): Uint8Array {
+function urlBase64ToUint8Array(base64String: string): ArrayBuffer {
   const padding = '='.repeat((4 - (base64String.length % 4)) % 4)
   const base64 = (base64String + padding).replace(/-/g, '+').replace(/_/g, '/')
   const rawData = window.atob(base64)
@@ -11,7 +11,7 @@ function urlBase64ToUint8Array(base64String: string): Uint8Array {
   for (let i = 0; i < rawData.length; ++i) {
     outputArray[i] = rawData.charCodeAt(i)
   }
-  return outputArray
+  return outputArray.buffer
 }
 
 function isIOS(): boolean {
