a6fd7992451e6378ec0833fb6aefcf6431cab299
sbadaspor <danielcostasilva.dcs@gmail.com>
2026-04-20 22:29:40 +0100

    Create vitest.config.ts

---
+++ vitest.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vitest/config'
+import path from 'path'
+
+export default defineConfig({
+  test: {
+    environment: 'node',
+    coverage: {
+      provider: 'v8',
+      include: ['src/lib/pontuacao.ts'],
+    },
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+})
